11fd5a869de9abc5bab22dd48ff1ace4b2e11082
Dip123Das <dip123das@rediffmail.com>
2026-04-24 12:05:16 +0530

    make full header logo prominent

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,75 +79,27 @@ export default function RootLayout({ children }: { children: ReactNode }) {
                 style={{
                   display: "flex",
                   alignItems: "center",
-                    gap: 10,
+                    gap: 0,
                   textDecoration: "none",
                   minWidth: 0,
                   width: "fit-content",
                   flexWrap: "nowrap",
                 }}
               >
-                <div
-                  style={{
-                    width: 68,
-                    height: 68,
-                    flexShrink: 0,
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
+                <div className="siteLogoFull">
                   <Image
-                    src="/logo-icon.png"
-                    alt="3Bigha icon"
-                    width={68}
-                    height={68}
+                    src="/logo.png"
+                    alt="3Bigha.com logo"
+                    width={360}
+                    height={120}
                     style={{
                       width: "100%",
                       height: "100%",
                       objectFit: "contain",
                       display: "block",
-                      borderRadius: 10,
                     }}
                     priority
                   />
-                </div>
-
-                <div
-                  style={{
-                    minWidth: 0,
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    lineHeight: 1.1,
-                  }}
-                >
-                  <div
-                    className="topBrandName"
-                    style={{
-                      fontSize: 22,
-                      lineHeight: 1,
-                      fontWeight: 900,
-                      letterSpacing: "-0.03em",
-                      color: "#0f172a",
-                      margin: 0,
-                    }}
-                  >
-                    3Bigha.com
-                  </div>
-
-                  <div
-                    className="topBrandTagline"
-                    style={{
-                      marginTop: 2,
-                      fontSize: 12,
-                      lineHeight: 1.2,
-                      color: "#475569",
-                      fontWeight: 700,
-                      letterSpacing: "0.01em",
-                    }}
-                  >
-                    Real Estate &amp; Construction Ecosystem
-                  </div>
                 </div>
               </Link>
             </div>
