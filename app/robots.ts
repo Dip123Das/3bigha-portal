0310a698de90e87cd3f72244f33085c8bfced251
Dipankar Das <vivek.abek@gmail.com>
2026-09-11 12:53:06 +0530

    Retire legacy SEO sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -42,7 +42,6 @@ export default function robots(): MetadataRoute.Robots {
     ],
     sitemap: [
       "https://3bigha.com/sitemap.xml",
-      "https://3bigha.com/seo-sitemap.xml",
       "https://3bigha.com/seo-sitemap-categories.xml",
     ],
   };
